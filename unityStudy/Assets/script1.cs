ae68f03615a307190a0983da096516fdc72af0dc
함태영 <tae0code@gmail.com>
2016-09-27 15:04:48 +0900

    6. 일정 시간 뒤 벽 사라지게 만들기 	a. 코루틴에 대하여 기술합니다. 	코루틴은 함수형식이고 작업을 처리할 때 시간을 두고 처리 할 수 	있게 해주는 함수이다. 	형식은 IEnumerator Coroutin(){ //code } 이고 	코루틴을 시작하게 해주는 함수는 StartCoroutine("Coroutine");이다.

--- unityStudy/Assets/script1.cs
+++ unityStudy/Assets/script1.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class script1 : MonoBehaviour {
 
+public class script1 : MonoBehaviour {
+	int count = 0;
 	// Use this for initialization
 	void Start () {
 		transform.position = new Vector3 (0.0f,0.0f,0.0f);
@@ -10,7 +11,20 @@ public class script1 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+//		if (count == 100) {
+//			Destroy(gameObject); // gameObject = 2d,3d object
+//		} else {
+//			float speed = 0.1f;
+//			transform.Translate (Vector3.left * speed);
+//			count++;
+//		}
+		StartCoroutine("Coroutine",2.0f);
 		float speed = 0.1f;
-		transform.Translate (Vector3.left*speed);
+		transform.Translate (Vector3.left * speed);
+
+	}
+	IEnumerator Coroutine(float time){
+		yield return  new WaitForSeconds (time);
+		Destroy (gameObject);
 	}
 }
