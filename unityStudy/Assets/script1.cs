08cad2006690e36b7f13a5880b9dbe484de39435
함태영 <tae0code@gmail.com>
2016-09-27 14:41:46 +0900

    5. 벽의 이동. a. start()함수는 Scene이 시작할 때 최초 1회 실행.     update()함수는 1프레임이 지나갈때마다 실행. b. public : 자신의 클래스 뿐만 아니라 외부 클래스에서 자기의 클래스를 생성하면 사용가능(외부 패키지에서도 패키지 추가만 해주면 사용가능)     private : 자신의 클래스 내부에서만 사용가능     protect : 퍼블릭과 비슷하지만 외부 패키지에서 상속 된 클래스 내부에서는 사용 가능하지만 외부에서는 사용 불가

--- unityStudy/Assets/script1.cs
+++ unityStudy/Assets/script1.cs
@@ -5,11 +5,12 @@ public class script1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		transform.position = new Vector3 (0.0f,0.0f,0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float speed = 0.1f;
+		transform.Translate (Vector3.left*speed);
 	}
 }
