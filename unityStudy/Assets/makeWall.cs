7223a9046dea583a0db192d814e62b1841a8ce95
함태영 <tae0code@gmail.com>
2016-09-29 00:30:44 +0900

    8. 스크립트 만들고 벽을 일정시간마다 구현

---
+++ unityStudy/Assets/makeWall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class makeWall : MonoBehaviour {
+	public GameObject prefab;
+	float time = 1.0f;
+	float CTime = 0;
+	float AddTime = 1.0f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > CTime) {
+			StartCoroutine ("makePrefab", time);
+			CTime = Time.time + AddTime;
+		}
+
+	}
+	IEnumerator makePrefab(float times){
+		yield return new WaitForSeconds (times);
+		Instantiate(prefab);
+	}
+}
