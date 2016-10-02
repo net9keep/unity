3058d736a679c291d2d3bbab86123f600706a2a3
함태영 <tae0code@gmail.com>
2016-10-02 20:01:57 +0900

    10. 벽 2개 생성(y축만큼 떨어져있음.)

--- unityStudy/Assets/makeWall_Prefab.cs
+++ unityStudy/Assets/makeWall_Prefab.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class makeWall : MonoBehaviour {
-	public GameObject prefab;
+public class makeWall_Prefab : MonoBehaviour {
+	public GameObject prefab,sprite;
 	float time = 1.0f;
 	float CTime = 0;
 	float AddTime = 1.0f;
-	Random rd = new Random();
 	// Use this for initialization
 	void Start () {
 		
@@ -24,6 +23,13 @@ public class makeWall : MonoBehaviour {
 	IEnumerator makePrefab(float times){
 		yield return new WaitForSeconds (times);
 		Instantiate(prefab, new Vector3(0,Random.Range(-10.0f,10.0f),0),Quaternion.identity);
+		float r = Random.Range (0, 1.9f);
+		float y = Random.Range (0, 3.0f);
+		if ((int)r == 0) {
+			Instantiate (sprite, new Vector3 (0, y, 0), Quaternion.identity);
+		} else {
+			Instantiate (sprite, new Vector3 (0, -1 * y, 0), Quaternion.identity); 
+		}
 
 	}
 }
