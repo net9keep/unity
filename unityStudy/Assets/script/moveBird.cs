3b6667a07dfc8e086fc182dee794b1456914f2e9
함태영 <tae0code@gmail.com>
2016-10-09 02:05:09 +0900

    2주차 2. 캐릭터 오브젝트가 시간에 따라 y좌표가 감소하고, 임의의 버튼을 누르는 동안은 y좌표가 증가하도록 구현합니다.

--- unityStudy/Assets/script/moveBird.cs
+++ unityStudy/Assets/script/moveBird.cs
@@ -10,6 +10,9 @@ public class moveBird : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey (KeyCode.UpArrow) == true)
+			transform.Translate (Vector3.up * 0.2f);
+		else
+			transform.Translate (Vector3.down * 0.2f);
 	}
 }
