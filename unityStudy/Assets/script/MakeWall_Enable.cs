bd1527855949c8e1e7977f0800be1e18047b58d5
함태영 <tae0code@gmail.com>
2016-10-17 15:31:28 +0900

    3주차 3. 일정 개수의 벽을 enable상태로 미리 만드는 스크립트 문제 : 해당  gameobject는 모두 enable상태이다.

---
+++ unityStudy/Assets/script/MakeWall_Enable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeWall_Enable : MonoBehaviour {
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+		for(int i=0; i<5;i++){
+			Instantiate (prefab, new Vector3 (0, Random.Range (-10.0f, 10.0f), 0), Quaternion.identity);
+			prefab.SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
