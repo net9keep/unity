3f07e377257532a8bd7e6cc5ffad7751c26eaaaf
함태영 <tae0code@gmail.com>
2016-11-07 01:07:35 +0900

    5.1

--- unityStudy/Assets/script/GameControll.cs
+++ unityStudy/Assets/script/GameControll.cs
@@ -23,7 +23,12 @@ public class GameControll : MonoBehaviour
 			GUI.Label (new Rect (10, 12, 100, 10), "점수:" + singleton.getInstance.score, style);
 		else {
 			style.fontSize = 100;
-			GUI.Label (new Rect (750, 350, 100, 10), "최종 점수:" + singleton.getInstance.score, style);
+			if (singleton.getInstance.score >= GetData ()){
+				GUI.Label (new Rect (750, 350, 100, 10), "new High Score:" + singleton.getInstance.score, style);
+				SaveData ();
+			}
+			else if(singleton.getInstance.score < GetData())
+				GUI.Label (new Rect (750, 350, 100, 10), "High Score:" + GetData(), style);
 			if (GUI.Button (new Rect (600, 450, 400, 100), "재시도")) {
 				singleton.getInstance.chk = 0; 
 				singleton.getInstance.score = 0;
@@ -31,6 +36,12 @@ public class GameControll : MonoBehaviour
 				UnityEngine.SceneManagement.SceneManager.LoadScene (number,UnityEngine.SceneManagement.LoadSceneMode.Single);
 			}
 		}
+	}
+	void SaveData(){
+		PlayerPrefs.SetInt("Score", singleton.getInstance.score);
+	}
+	int GetData(){
+		return PlayerPrefs.GetInt ("Score");
 	}
 	public void Crush(){
 		
