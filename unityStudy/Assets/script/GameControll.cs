6f70aec24f1c51b5af0c9a8e38e1666b44ebb93f
함태영 <tae0code@gmail.com>
2016-11-07 00:35:25 +0900

    4.2

--- unityStudy/Assets/script/GameControll.cs
+++ unityStudy/Assets/script/GameControll.cs
@@ -13,7 +13,13 @@ public class GameControll : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		
+	}
+	void OnGUI(){
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 30;
+		style.alignment = TextAnchor.MiddleCenter;
+		GUI.Label(new Rect(10, 12, 100, 10), "점수:" + singleton.getInstance.score, style);
 	}
 	public void Crush(){
 		
--- unityStudy/Assets/script/moveBird.cs
+++ unityStudy/Assets/script/moveBird.cs
@@ -14,7 +14,7 @@ public class MoveBird : MonoBehaviour {
 			transform.Translate (Vector3.up * 0.1f);
 		else
 			transform.Translate (Vector3.down * 0.1f);
-		if (!(transform.position.y > -7 && transform.position.y < 7))
+		if (!(transform.position.y > -8 && transform.position.y < 8))
 			singleton.getInstance.chk = 1;
 		switch ((int)transform.position.y) {
 		case 8:
